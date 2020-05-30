f1478c1344c9e3817af2c44db3638230fec2b6bc
DIGISTALE FINANS <broucedrickatse@gmail.com>
2020-05-30 07:41:39 +0000

    itineraire

--- lib/screen/Itinenaire.dart
+++ lib/screen/Itinenaire.dart
@@ -3,58 +3,36 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class Itineraire extends StatefulWidget {
+class Itineraire extends StatelessWidget {
   final String title;
   final String url;
 
 
   Itineraire(this.url, this.title);
 
-  @override
-  _ItineraireState createState() => _ItineraireState();
-}
-
-class _ItineraireState extends State<Itineraire> {
-  String page;
-  Completer<WebViewController> _controller = Completer<WebViewController>();
-
-  @override
-  void initState() {
-    print(widget.url);
-    setState(() {
-      page = "";
-    });
-    super.initState();
-  }
+  final Completer<WebViewController> _controller =
+  Completer<WebViewController>();
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 400,
-            width: 400,
-            child: WebView(
+      body: WebView(
               javascriptMode: JavascriptMode.unrestricted,
               initialUrl: "<html>\n"
                   "       <head>\n\n"
                   "       </head>\n"
                   "      <body style=\"padding: 0\">\n"
-                  "        <iframe src=\"${widget.url}\" width=\"100%\" height=\"100%\"/>\n"
+                  "        <iframe src=\"${url}\" width=\"100\" height=\"100\"/>\n"
                   "    </body>\n"
                   "</html>",
               onWebViewCreated: (WebViewController webViewController) {
                 _controller.complete(webViewController);
               },
             ),
-          )
-        ],
-      )
     );
   }
 }
